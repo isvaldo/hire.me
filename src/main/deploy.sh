4498257774c0303bf4e3cd77dcaa2744a70e9cfe
= <isvaldo.fernandes@gmail.com>
2016-04-10 19:24:41 -0300

    bugfix,

--- src/main/deploy.sh
+++ src/main/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mvn package docker:build
 # Delete all containers
 docker rm $(docker ps -a -q)
 # Delete all images
 docker rmi $(docker images -q)
+mvn package docker:build
 docker run -d --name redis -p 6379:6379 redis
 docker run -ti --rm --link redis:redis -p 81:8080 shortener/shortener
