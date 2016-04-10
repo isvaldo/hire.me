81dd4dd6deee70d3c985b98ddce6b25c50f3fe16
= <isvaldo.fernandes@gmail.com>
2016-04-10 19:17:40 -0300

    Change deploy file, rm old imgs

--- src/main/deploy.sh
+++ src/main/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 mvn package docker:build
-docker run --name redis -d redis
-docker run --name shortener -d -p 81:8080 -t shortener/shortener
+# Delete all containers
+docker rm $(docker ps -a -q)
+# Delete all images
+docker rmi $(docker images -q)
+docker run -d --name redis -p 6379:6379 redis
+docker run -ti --rm --link redis:redis -p 81:8080 shortener/shortener
