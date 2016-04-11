4cc6e033259a718da19207351ef2e9721016c609
= <isvaldo.fernandes@gmail.com>
2016-04-10 22:04:44 -0300

    martelando deploy com ip static ):

--- src/main/deploy.sh
+++ src/main/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #container do redis [Required by testes]
 docker run -d --name redis -p 6379:6379 redis
-#Pega ip na configuração do container do redis
-export REDIS_PORT_6379_TCP_ADDR = docker inspect --format '{{ .NetworkSettings.IPAddress }}' redis
-#Testes & dcoker img
+docker start
+#Ainda não entendi muito bem como se comunicar entre os containers
+#então estou martelando esse ip, mas pelo menos, redis e sprinb boot
+#estão separados
+export REDIS_PORT_6379_TCP_ADDR=172.17.0.16
+#Testes & docker img
 mvn package docker:build
 #container da aplicacao no ar
 docker run -ti --rm --link redis:redis -p 81:8080 shortener/shortener
