461a11aee9e29585f695007dab51b5474058bf63
= <isvaldo.fernandes@gmail.com>
2016-04-10 21:39:22 -0300

    Ajustando a comunicação entre os containers

--- src/main/deploy.sh
+++ src/main/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Delete all containers
-docker rm $(docker ps -a -q)
-# Delete all images
-docker rmi $(docker images -q)
-mvn package docker:build
+#container do redis [Required by testes]
 docker run -d --name redis -p 6379:6379 redis
+#Pega ip na configuração do container do redis
+export REDIS_PORT_6379_TCP_ADDR = docker inspect --format '{{ .NetworkSettings.IPAddress }}' redis
+#Testes & dcoker img
+mvn package docker:build
+#container da aplicacao no ar
 docker run -ti --rm --link redis:redis -p 81:8080 shortener/shortener
