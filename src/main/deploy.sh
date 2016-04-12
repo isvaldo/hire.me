88232b9369eae8d26493565dc76f8ef9615b389c
= <isvaldo.fernandes@gmail.com>
2016-04-11 21:41:36 -0300

    trocando porta

--- src/main/deploy.sh
+++ src/main/deploy.sh
@@ -7,4 +7,4 @@ export REDIS_PORT_6379_TCP_ADDR=$(docker inspect --format '{{ .NetworkSettings.I
 #Testes & docker img
 mvn package docker:build
 #container da aplicacao no ar
-docker run -d --link redis:redis -p 81:8080 shortener/shortener
+docker run -d --link redis:redis -p 80:8080 shortener/shortener
